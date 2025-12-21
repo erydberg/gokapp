73f53c4afc1a8b9d9a813e0db71e945748b6c406
Erik Rydberg <erydberg@gmail.com>
2025-12-21 22:42:28 +0100

    Added support viewing patrol details from the start finish-page and for editing and deleting a score from start-finish-page

--- src/main/java/se/scouttavling/gokapp/startfinish/StartFinishController.java
+++ src/main/java/se/scouttavling/gokapp/startfinish/StartFinishController.java
@@ -1,24 +1,29 @@
 package se.scouttavling.gokapp.startfinish;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import se.scouttavling.gokapp.patrol.Patrol;
 import se.scouttavling.gokapp.patrol.PatrolService;
 import se.scouttavling.gokapp.patrol.Status;
 import se.scouttavling.gokapp.patrol.StatusCounter;
+import se.scouttavling.gokapp.score.Score;
+import se.scouttavling.gokapp.score.ScoreService;
 
 import java.util.List;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/startfinish")
+@RequiredArgsConstructor
 public class StartFinishController {
 
-    @Autowired
-    private PatrolService patrolService;
+
+    private final PatrolService patrolService;
+    private final ScoreService scoreService;
 
     @GetMapping
     public String loadStartAndFinish(@RequestParam(required = false) String sort,
@@ -60,13 +65,68 @@ public class StartFinishController {
         patrolService.updateStatus(id, Status.REGISTERED);
     }
 
-    @GetMapping("/viewpatrol/{id}")
+
+
+    @GetMapping("/patrol/{id}")
     public String viewPatrol(@PathVariable Integer id, Model model) {
         Patrol patrol = patrolService.getPatrolById(id).orElseThrow(() -> new IllegalArgumentException("Station not found"));
+        List<Score> scores = scoreService.findByPatrol(id);
 
         model.addAttribute("patrol", patrol);
-        model.addAttribute("backurl", "/startfinish");
-        return "viewpatrol";
+        model.addAttribute("scores", scores);
+
+        return "startfinish_patrol";
+    }
+
+
+    /**
+     * Edit an existing score from start_finish
+     */
+    @GetMapping("/score/{id}")
+    public String editScore(@PathVariable Integer id, Model model) {
+        Score score = scoreService.findByIdWithPatrolAndStation(id)
+                .orElseThrow(() -> new IllegalArgumentException("Score not found"));
+
+        model.addAttribute("score", score);
+        //model.addAttribute("stations", stationService.getAll());
+        return "startfinish_correct_score";
+    }
+
+
+    /**
+     * Update or delete a score
+     */
+    @PostMapping("/score")
+    public String updateOrDelete(@ModelAttribute("score") Score score, @RequestParam String action, RedirectAttributes redirectAttributes) {
+
+        Score scoreFromDb = scoreService.findByIdWithPatrolAndStation(score.getId()).orElseThrow(() -> new IllegalArgumentException("Score not found"));
+
+        if ("delete".equals(action)) {
+            System.out.println("score " + score);
+            Patrol patrol = scoreFromDb.getPatrol();
+            System.out.println("patrol " + patrol);
+
+            patrol.getScores().remove(scoreFromDb); // triggers orphan removal
+            patrolService.save(patrol);    // persist the change
+
+            redirectAttributes.addFlashAttribute("confirmmsg", "Poängen borttagen");
+            return "redirect:/startfinish/patrol/" + scoreFromDb.getPatrol().getPatrolId();
+        }
+
+        scoreService.save(score);
+
+        if (score.isVisitedWaypoint()) {
+            redirectAttributes.addFlashAttribute("confirmmsg",
+                    "Ändrat till " + scoreFromDb.getPatrol().getPatrolName() + " har passerat kontrollen.");
+        } else {
+            redirectAttributes.addFlashAttribute("confirmmsg",
+                    "Ändrat till " + score.getScorePoint() + " poäng och " + score.getStylePoint() +
+                            " stilpoäng för " + scoreFromDb.getPatrol().getPatrolName() +
+                            " från " + scoreFromDb.getPatrol().getTroop() + ".");
+        }
+
+
+        return "redirect:/startfinish/patrol/" + score.getPatrol().getPatrolId();
     }
 
 
