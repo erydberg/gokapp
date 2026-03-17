6981a4bc060515e05ad19cac1832f789561efe07
Erik Rydberg <erydberg@gmail.com>
2026-03-17 19:17:10 +0100

    Steps to avoid double registration of patrols in the public register page using page redirect

--- src/main/java/se/scouttavling/gokapp/configuration/RegistrationConfigController.java
+++ src/main/java/se/scouttavling/gokapp/configuration/RegistrationConfigController.java
@@ -4,7 +4,6 @@ package se.scouttavling.gokapp.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
